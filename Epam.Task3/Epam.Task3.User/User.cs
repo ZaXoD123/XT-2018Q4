3d8584ad90fdfb1f4ccd4b965360751b416dfc69
Zaxod123 <nikulinnikolai@mail.ru>
2018-12-03 21:46:09 +0400

    Task3 v0.1.1

---
+++ Epam.Task3/Epam.Task3.User/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Epam.Task3.User
+{
+    internal class User
+    {
+        public User(string lastName, string name, string patternalName, DateTime birthDate)
+        {
+            if (birthDate >= DateTime.Now.Date || birthDate.Year <= 1850) throw new Exception("Wrong date!");
+            FullName = (name, lastName, patternalName);
+            BirthDate = birthDate.Date;
+        }
+
+        public (string name, string lastName, string patternalName) FullName { get; }
+        public DateTime BirthDate { get; }
+
+        public int GetAges()
+        {
+            return (int) ((DateTime.Now - BirthDate).Days / 365.2425);
+        }
+    }
+}
