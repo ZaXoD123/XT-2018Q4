30024eea91417c508d26f92b03bdd793c1d860de
Zaxod123 <nikulinnikolai@mail.ru>
2018-12-05 23:43:09 +0400

    Task3

---
+++ Epam.Task3/Epam.Task3.Game/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.Game
+{
+    public interface IGameObject
+    {
+        bool IsStatic { get; }
+    }
+
+    public interface INPC 
+    {
+        bool IsStatic { get; }
+
+        void HitAction(IPlayer player);
+    }
+
+    public interface IPlayer
+    {
+        uint Health { get; }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.Game/Map.cs
@@ -0,0 +1,49 @@
+﻿namespace Epam.Task3.Game
+{
+    public abstract class Map
+    {
+        private IPlayer player;
+        private IGameObject[,] desk;
+
+        public Map(uint monstersPercent = 30, uint bonusPercent = 20, uint length = 10)
+        {
+            var temp = this.MapGenerator(monstersPercent, bonusPercent, length);
+            this.desk = temp.Item1;
+            this.player = temp.Item2;
+            this.MapLength = length;
+        }
+
+        public uint MapLength { get; }
+
+        public bool Move(IGameObject gameObject, int newX, int newY)
+        {
+            if (this.desk[newX, newY] != null && (gameObject.IsStatic || this.desk[newX, newY].IsStatic))
+            {
+                return false;
+            }
+
+            this.MoveObjectAction(gameObject, newX, newY);
+            this.desk[newX, newY] = gameObject;
+            return true;
+        }
+
+        public bool MoveObjectAction(IGameObject gameObject, int newX, int newY)
+        {
+            if (gameObject is IPlayer)
+            {
+                (this.desk[newX, newY] as INPC).HitAction(gameObject as IPlayer);
+                return true;
+            }
+
+            if (this.desk[newX, newY] is IPlayer)
+            {
+                (gameObject as INPC).HitAction(this.player);
+                return true;
+            }
+
+            return true;
+        }
+
+        protected abstract (IGameObject[,], IPlayer) MapGenerator(uint monsters, uint bonuses, uint length);
+    }
+}
