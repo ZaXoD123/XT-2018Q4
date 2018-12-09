080524b42ccc0b90dc8e9e28baf3965ad6c5a2b3
Zaxod123 <nikulinnikolai@mail.ru>
2018-12-09 21:13:33 +0400

    Task4 0.1

---
+++ Epam.Task4/Epam.Task4.Lost/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task4.Lost
+{
+    class Program
+    {
+        static T LastMethod<T>(List<T> persons)
+        {
+            int current = 1;
+            while (persons.Count > 1)
+            {
+                current = current % persons.Count;
+                persons.RemoveAt(current++);
+            }
+            
+            return persons[0];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter count of persons: ");
+            inputLabel:
+            if (!int.TryParse(Console.ReadLine(), out var input) || input < 1)
+            {
+                Console.WriteLine($"Wrong value. Try again {input}");
+                goto inputLabel;
+            }
+
+            List<int> persons = new List<int>(Enumerable.Range(1,input));
+            
+            Console.WriteLine(LastMethod(persons));
+        }
+    }
+}
---
+++ Epam.Task4/Epam.Task4.WordFrequency/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task4.WordFrequency
+{
+    internal class Program
+    {
+        private static void GetWordsCount(Dictionary<string, int> words, string message)
+        {
+            if (message == null)
+            {
+                throw new Exception("Wrong string");
+            }
+
+            foreach (var item in message.ToLower().Split('.', ' '))
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    continue;
+                }
+
+                if (words.ContainsKey(item))
+                {
+                    words[item] = words[item] + 1;
+                }
+                else
+                {
+                    words.Add(item, 1);
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            var a = new Dictionary<string, int>();
+            startInput:
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Wrong values. Try again");
+                goto startInput;
+            }
+
+            GetWordsCount(a, input);
+            
+            foreach (var item in a.Keys) Console.WriteLine($"{item} {a[item]}");
+        }
+    }
+}
