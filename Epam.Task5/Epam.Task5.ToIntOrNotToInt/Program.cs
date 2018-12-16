e7c08de4191a6d90515ca3537d1b12b08d6de5b8
Zaxod123 <nikulinnikolai@mail.ru>
2018-12-16 22:53:13 +0400

    fix

--- Epam.Task5/Epam.Task5.ToIntOrNotToInt/Program.cs
+++ Epam.Task5/Epam.Task5.ToIntOrNotToInt/Program.cs
@@ -8,12 +8,17 @@ namespace Epam.Task5.ToIntOrNotToInt
         private static bool IsInt(string str)
         {
             var template = new Regex(@"(\d*\.*\d+E[+,-]\d+)|(\d*\.*\d+)");
-            return template.IsMatch(str) & (template.Replace(str, string.Empty) == string.Empty);
+            return template.IsMatch(str) & template.Matches(str).Count == 1 & (template.Replace(str, string.Empty) == string.Empty);
         }
 
         private static void Main(string[] args)
         {
-            Console.WriteLine(IsInt(Console.ReadLine()));
+            Console.WriteLine($"12.12E+20: {IsInt("12.12E+20")}");
+            Console.WriteLine($"12.E+20: {IsInt("12.E+20")}");
+            Console.WriteLine($"12.12E+: {IsInt("12.12E+")}");
+            Console.WriteLine($"1234E+1: {IsInt("1234E+1")}");
+            Console.WriteLine($"1234: {IsInt("1234")}");
+            Console.WriteLine($"123.123.123: {IsInt("123.123.123")}");
         }
     }
 }
